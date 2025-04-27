ea089172187d2665c3bdf40c2dbd3fcbc634fb99
Kram-V <markanthonyvivar24@gmail.com>
2025-04-28 04:00:01 +0800

    added skeleton loader and api integrated

--- src/views/widgets/WidgetsDropdown.js
+++ src/views/widgets/WidgetsDropdown.js
@@ -1,83 +1,131 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 
-import {
-  CRow,
-  CCol,
-  CDropdown,
-  CDropdownMenu,
-  CDropdownItem,
-  CDropdownToggle,
-  CWidgetStatsA,
-} from '@coreui/react'
+import { CRow, CCol } from '@coreui/react'
 import { getStyle } from '@coreui/utils'
-import { CChartBar, CChartLine } from '@coreui/react-chartjs'
 import CIcon from '@coreui/icons-react'
 import { cilCheckCircle, cilHistory, cilPeople, cilShieldAlt, cilWarning } from '@coreui/icons'
 import Card from '../../components/Card'
 
+import ContentLoader from 'react-content-loader'
+import { getStats } from '../../http/dashboard'
+
 const WidgetsDropdown = (props) => {
-  const widgetChartRef1 = useRef(null)
-  const widgetChartRef2 = useRef(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [totalAdminUsers, setTotalAdminUsers] = useState(0)
+  const [totalNonAdminUsers, setTotalNonAdminUsers] = useState(0)
+
+  const getAllStats = () => {
+    setIsLoading(true)
+
+    getStats()
+      .then((res) => {
+        setTotalAdminUsers(res.data.total_admin_users)
+        setTotalNonAdminUsers(res.data.total_non_admin_users)
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false))
+  }
 
   useEffect(() => {
-    document.documentElement.addEventListener('ColorSchemeChange', () => {
-      if (widgetChartRef1.current) {
-        setTimeout(() => {
-          widgetChartRef1.current.data.datasets[0].pointBackgroundColor = getStyle('--cui-primary')
-          widgetChartRef1.current.update()
-        })
-      }
-
-      if (widgetChartRef2.current) {
-        setTimeout(() => {
-          widgetChartRef2.current.data.datasets[0].pointBackgroundColor = getStyle('--cui-info')
-          widgetChartRef2.current.update()
-        })
-      }
-    })
-  }, [widgetChartRef1, widgetChartRef2])
+    getAllStats()
+  }, [])
 
   return (
     <CRow className={props.className} xs={{ gutter: 4 }}>
       <CCol sm={6} xl={4} xxl={3}>
-        <Card
-          total={50}
-          subText="Total Student Reports"
-          icon={<CIcon icon={cilWarning} size="xl" />}
-          bgColor="bg-danger"
-        />
+        {isLoading ? (
+          <ContentLoader
+            viewBox="0 0 100% 110"
+            height="110"
+            width="100%"
+            style={{ position: 'relative', top: '-1px' }}
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="110" />
+          </ContentLoader>
+        ) : (
+          <Card
+            total={50}
+            subText="Total Student Reports"
+            icon={<CIcon icon={cilWarning} size="xl" />}
+            bgColor="bg-danger"
+          />
+        )}
       </CCol>
       <CCol sm={6} xl={4} xxl={3}>
-        <Card
-          total={32}
-          subText="Total Pending Reports"
-          icon={<CIcon icon={cilHistory} size="xl" />}
-          bgColor="bg-warning"
-        />
+        {isLoading ? (
+          <ContentLoader
+            viewBox="0 0 100% 110"
+            height="110"
+            width="100%"
+            style={{ position: 'relative', top: '-1px' }}
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="110" />
+          </ContentLoader>
+        ) : (
+          <Card
+            total={32}
+            subText="Total Pending Reports"
+            icon={<CIcon icon={cilHistory} size="xl" />}
+            bgColor="bg-warning"
+          />
+        )}
       </CCol>
       <CCol sm={6} xl={4} xxl={3}>
-        <Card
-          total={32}
-          subText="Total Resolved Reports"
-          icon={<CIcon icon={cilCheckCircle} size="xl" />}
-          bgColor="bg-success"
-        />
+        {isLoading ? (
+          <ContentLoader
+            viewBox="0 0 100% 110"
+            height="110"
+            width="100%"
+            style={{ position: 'relative', top: '-1px' }}
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="110" />
+          </ContentLoader>
+        ) : (
+          <Card
+            total={32}
+            subText="Total Resolved Reports"
+            icon={<CIcon icon={cilCheckCircle} size="xl" />}
+            bgColor="bg-success"
+          />
+        )}
       </CCol>
       <CCol sm={6} xl={4} xxl={3}>
-        <Card
-          total={5}
-          subText="Total Admin Users"
-          icon={<CIcon icon={cilShieldAlt} size="xl" />}
-        />
+        {isLoading ? (
+          <ContentLoader
+            viewBox="0 0 100% 110"
+            height="110"
+            width="100%"
+            style={{ position: 'relative', top: '-1px' }}
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="110" />
+          </ContentLoader>
+        ) : (
+          <Card
+            total={totalAdminUsers}
+            subText="Total Admin Users"
+            icon={<CIcon icon={cilShieldAlt} size="xl" />}
+          />
+        )}
       </CCol>
       <CCol sm={6} xl={4} xxl={3}>
-        <Card
-          total={25}
-          subText="Total Staff Users"
-          icon={<CIcon icon={cilPeople} size="xl" />}
-          bgColor="bg-info"
-        />
+        {isLoading ? (
+          <ContentLoader
+            viewBox="0 0 100% 110"
+            height="110"
+            width="100%"
+            style={{ position: 'relative', top: '-1px' }}
+          >
+            <rect x="3" y="3" rx="10" ry="10" width="100%" height="110" />
+          </ContentLoader>
+        ) : (
+          <Card
+            total={totalNonAdminUsers}
+            subText="Total Staff Users"
+            icon={<CIcon icon={cilPeople} size="xl" />}
+            bgColor="bg-info"
+          />
+        )}
       </CCol>
     </CRow>
   )
