4d025bbe9f9bc8f15f1eb618741a541402a94ec7
Kram-V <markanthonyvivar24@gmail.com>
2025-05-04 13:38:47 +0800

    removed dark mode

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -49,10 +49,10 @@ const AppHeader = () => {
             </CDropdownToggle>
           </CDropdown>
 
-          <li className="nav-item py-1">
+          {/* <li className="nav-item py-1">
             <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li>
-          <CDropdown variant="nav-item" placement="bottom-end">
+          </li> */}
+          {/* <CDropdown variant="nav-item" placement="bottom-end">
             <CDropdownToggle caret={false}>
               {colorMode === 'dark' ? (
                 <CIcon icon={cilMoon} size="lg" onClick={() => setColorMode('light')} />
@@ -60,7 +60,7 @@ const AppHeader = () => {
                 <CIcon icon={cilSun} size="lg" onClick={() => setColorMode('dark')} />
               )}
             </CDropdownToggle>
-          </CDropdown>
+          </CDropdown> */}
           <li className="nav-item py-1">
             <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
           </li>
